06835ad65bd9598d781d3166fa86b294c6fb4a98
Alinda <AlindaHeng@gmail.com>
2021-03-29 00:07:28 -0700

    Simple program that responds to get and post requests

---
+++ fwd_be.py
@@ -0,0 +1,23 @@
+"""
+This app returns the length of a given string.
+
+"""
+from flask import Flask, request
+from flask_restful import Resource, Api
+
+app = Flask(__name__)
+api = Api(app)
+
+class ReturnLength(Resource):
+    def get(self):
+    	return "Greetings planet"
+
+    def post(self):
+    	data = request.form['num_input']
+    	return len(data)
+
+api.add_resource(ReturnLength, '/')
+
+if __name__ == '__main__':
+    app.run(debug=True)
+
